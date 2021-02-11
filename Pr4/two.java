44d5f2b7d73bc9d796c0038b85ee88d68f0d9104
Ch3nske <77534973+Ch3nske@users.noreply.github.com>
2021-02-11 12:59:51 +0300

    Update two.java

--- Pr4/two.java
+++ Pr4/two.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class two {
     public static void main(String[] args) {
-        int arr[] = new int[10];
+        int arr[] = new int[20];
         for(int i = 0; i <  arr.length; i++) {
             arr[i] =  (int)(Math.random() * 30);
             System.out.print(arr[i] + "  ");
