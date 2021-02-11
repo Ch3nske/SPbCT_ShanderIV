4c04b15e410bddb6d88cfe95adfd7736f88c4330
Ch3nske <77534973+Ch3nske@users.noreply.github.com>
2021-02-11 12:59:48 +0300

    Update one.java

--- Pr4/one.java
+++ Pr4/one.java
@@ -31,13 +31,18 @@ public class one {
         }
         return arr;
     }
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         int arr[] = {1, 3, 2, 7, 5, 4, 8, 10, 9, 13, 15 , 14};
-
+        System.out.println("Исходный массив: " + Arrays.toString(arr) + "\n");
+        int[] copyArr=null;
+        copyArr = copyFullArrayUsingArrayCopyOf(arr);
+        System.out.println("Полное копирование массива: " + Arrays.toString(copyArr) + "\n");
         oddEvenSort(arr, arr.length);
-
         System.out.println(Arrays.toString(arr));
+    }
+    /*Метод Get */
+    private static int[] copyFullArrayUsingArrayCopyOf(int[] arr) {
+        return Arrays.copyOf(arr, arr.length);
     }
 }
 
